ffc311afbebe27ec52d1ceb0865614528bc74e68
Andras Mitzki <andras.mitzki@balabit.com>
2016-05-23 09:30:40 +0200

    Elastic5: Set system variable vm.max_map_count at start time
    Signed-off-by: Andras Mitzki <andras.mitzki@balabit.com>

--- build-images.sh
+++ build-images.sh
@@ -2,6 +2,8 @@
 
 prefix="micek"
 directories="elastic1 elastic2 elastic5 syslogng38-elastic1 syslogng38-elastic2 syslogng38-elastic5"
+echo 'set system variable, needed by elasticsearch 5, see URL: [https:/github.com/docker-library/elasticsearch/issues/98/]'
+sudo sysctl -w vm.max_map_count=262144
 
 for directory in $directories ; do
     docker build --no-cache=False -t $prefix/$directory $directory/
--- test-elasticsearch.sh
+++ test-elasticsearch.sh
@@ -1,8 +1,5 @@
 #!/bin/bash -x
 
-echo 'set system variable, needed by elasticsearch 5, see URL: [https:/github.com/docker-library/elasticsearch/issues/98/]'
-sudo sysctl -w vm.max_map_count=262144
-
 elastic_nodes="192.168.10.1 192.168.10.2 192.168.10.3"
 
 for elastic_node in $elastic_nodes; do
